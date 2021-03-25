b0fe83f23e21a82b9d4c59ecb87bc85551726b8e
abdelwahedmodest <>
2021-03-25 10:43:30 +0000

    second modif in config

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     */
 
 		'default' => env('DB_CONNECTION', 'mysql'),
-
+#testone
     /*
     |--------------------------------------------------------------------------
     | Database Connections
